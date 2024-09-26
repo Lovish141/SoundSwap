312c5b5c65a296e66b7f4847442104c063cbfd5c
Lovish Sharma <slovish11sharma@gmail.com>
2024-09-26 17:43:15 +0530

    Fixed Cors issue for frontend domain

--- server/index.js
+++ server/index.js
@@ -9,20 +9,14 @@ import authRoutes from './routes/authRoutes.js';
 import playlistRoutes from './routes/playListRoutes.js'
 import sessionRoutes from './routes/sessionRoutes.js'
 const app=express();
-const allowedDomains = ['https://sound-swap-frontend.vercel.app'];
 
-const corsOptions=(origin,callback)=>{
-    
-        if (!origin) return callback(null, true); // Allow non-browser requests (Postman, etc.)
-        
-        if (allowedDomains.indexOf(origin) !== -1) {
-          callback(null, true); // Allow requests from allowed domains
-        } else {
-          callback(new Error('Not allowed by CORS')); // Block other domains
-        }
+app.use(cors(
+    {
+        origin: 'https://sound-swap-frontend.vercel.app', // Allow requests from this origin
+        methods: ['GET', 'POST', 'PUT', 'DELETE'], // Specify allowed HTTP methods
+        allowedHeaders: ['Content-Type', 'Authorization'], // Specify allowed headers
       }
-
-app.use(cors(corsOptions));
+));
 app.use(express.json());
 
 app.use('/api/auth',authRoutes);
