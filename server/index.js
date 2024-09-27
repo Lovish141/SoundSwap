6f66f85ffe3f617a88fe36ce48663bcab0465ab2
Lovish Sharma <slovish11sharma@gmail.com>
2024-09-27 18:00:42 +0530

    Fix:Localhost cors issue

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ const app=express();
 
 app.use(cors(
     {
-        origin: 'https://sound-swap-frontend.vercel.app', // Allow requests from this origin
+        origin: ['https://sound-swap-frontend.vercel.app','http://localhost:5173/'], // Allow requests from this origin
         methods: ['GET', 'POST', 'PUT', 'DELETE'], // Specify allowed HTTP methods
         allowedHeaders: ['Content-Type', 'Authorization'], // Specify allowed headers
       }
